d2ef4ff2a0322ec374bd8a3ac39b54d86568c721
Tinsley Huo <tinsleyhuo@196.134.182.163.naturalwireless.com>
2020-07-12 18:23:49 -0400

    5443

--- script_sub.js
+++
@@ -1,9 +0,0 @@
-console.log("Tinsley!")
-    $(document).ready(function () {
-        $('#about').each(function () {
-            $(this).attr('data-buzz', $(this).text());
-        });
-        $('#work').each(function () {
-            $(this).attr('data-buzz', $(this).text());
-        });
-    });
